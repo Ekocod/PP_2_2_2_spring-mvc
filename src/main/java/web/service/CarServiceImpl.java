00fc6c60cc7cb94236547fee5b55539552f2037a
Eko <ekoslambek@gmail.com>
2022-11-23 23:45:12 +0600

    codestyle

--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -8,14 +8,14 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
-public class CarServiceImpl implements CarService{
+public class CarServiceImpl implements CarService {
     private final List<Car> carList;
 
     public CarServiceImpl() {
         carList = new ArrayList<>();
-        carList.add(new Car(1,"Ford", "Mustang"));
+        carList.add(new Car(1, "Ford", "Mustang"));
         carList.add(new Car(2, "Porsche", "Cayenne"));
-        carList.add(new Car(3,"Range Rover", "SDV6"));
+        carList.add(new Car(3, "Range Rover", "SDV6"));
         carList.add(new Car(4, "Volkswagen", "Tuareg"));
         carList.add(new Car(5, "BMW", "X6"));
     }
